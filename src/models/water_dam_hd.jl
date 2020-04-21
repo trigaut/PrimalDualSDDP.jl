01f07d63fa09edf97f3b21c424928d3a6821e15a
trigaut <tristan.rigaut@gmail.com>
2020-04-21 11:10:11 +0200

    trailing white spaces

--- src/models/water_dam_hd.jl
+++ src/models/water_dam_hd.jl
@@ -1,26 +1,26 @@
 mutable struct WaterDamModel <: HazardDecisionModel
-	Δt::Float64
-	capacity::Float64
-	umax::Float64
-	csell::Vector{Float64}
-	ξ
-	πξ
-	ξs
-	fₜ
+    Δt::Float64
+    capacity::Float64
+    umax::Float64
+    csell::Vector{Float64}
+    ξ
+    πξ
+    ξs
+    fₜ
 end
 
 function WaterDamModel(Δt::Float64, capacity::Float64, 
-					   umax::Float64, csell::Vector{Float64}, 
-					   waterfall_scenarios::Array{Float64,2}, 
-					   bins::Int)
-	
-	ξ, πξ = discrete_white_noise(waterfall_scenarios, bins)
+                       umax::Float64, csell::Vector{Float64}, 
+                       waterfall_scenarios::Array{Float64,2}, 
+                       bins::Int)
+    
+    ξ, πξ = discrete_white_noise(waterfall_scenarios, bins)
 
-	function fₜ(t, xₜ, uₜ₊₁, ξₜ₊₁)
-		return [xₜ[1] - uₜ₊₁[1] + ξₜ₊₁[1]]
-	end
+    function fₜ(t, xₜ, uₜ₊₁, ξₜ₊₁)
+        return [xₜ[1] - uₜ₊₁[1] + ξₜ₊₁[1]]
+    end
 
-	return WaterDamModel(Δt, capacity, umax, csell, 
-				  		 ξ, πξ, waterfall_scenarios[:,:,:], fₜ)
+    return WaterDamModel(Δt, capacity, umax, csell, 
+                         ξ, πξ, waterfall_scenarios[:,:,:], fₜ)
 end
 
--- src/utilities.jl
+++ src/utilities.jl
@@ -1,9 +1,9 @@
 function variables_product(args...)
-	dim_variable = length(args)
-	products = Base.product(args...)
-	products_array = fill(0., length(products), dim_variable)
-	for (i, product) in enumerate(products)
-		products_array[i,:] .= [product...]  
-	end 
-	products_array
+    dim_variable = length(args)
+    products = Base.product(args...)
+    products_array = fill(0., length(products), dim_variable)
+    for (i, product) in enumerate(products)
+        products_array[i,:] .= [product...]  
+    end 
+    products_array
 end
