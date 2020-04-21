3c49a33f2901c24a70bc7ad16aebc6644e53eab2
trigaut <tristan.rigaut@gmail.com>
2020-04-21 15:58:08 +0200

    DRY dualization

--- src/dualization.jl
+++ src/dualization.jl
@@ -1,5 +1,4 @@
-function auto_dual_bellman_operator(dhm::DecisionHazardModel, t::Int, lip::Real)
-    m = bellman_operator(dhm, t)
+function auto_dual_bellman_operator(m::JuMP.Model, lip::Real)
     md = dualize(m; dual_names = DualNames("", ""))
 
     nx = length(m[:xₜ]) 
@@ -36,39 +35,8 @@ function auto_dual_bellman_operator(dhm::DecisionHazardModel, t::Int, lip::Real)
     return md
 end
 
-function auto_dual_bellman_operator(m::JuMP.Model, lip::Real)
-    md = dualize(m; dual_names = DualNames("", ""))
-
-    nx = length(m[:xₜ]) 
-    info =  VariableInfo(false, NaN, false, NaN, false, NaN, 
-                         false, NaN, false, false)
-
-    md[:μₜ] = VariableRef[]
-    for var in name.(m[:xₜ])
-        con = JuMP.constraint_by_name(md, var)
-        adjointvar = add_variable(md, build_variable(error, info), "μ"*var)
-        push!(md[:μₜ], adjointvar)
-        set_normalized_coefficient(con, adjointvar, 1.)
-    end
-
-    xₜ₊₁uniquenames = unique(cat(map(x -> name.(x), m[:xₜ₊₁])..., dims=1))
-
-    info =  VariableInfo(true, -lip, true, lip, false, NaN, 
-                         false, NaN, false, false)
-
-    for var in xₜ₊₁uniquenames
-        con = JuMP.constraint_by_name(md, var)
-        adjointvar = add_variable(md, build_variable(error, info), "μ"*var)
-        set_normalized_coefficient(con, adjointvar, -1.)
-    end
-    
-    md[:μₜ₊₁] = Array{Array{VariableRef,1},1}()
-    for var_array in m[:xₜ₊₁]
-        push!(md[:μₜ₊₁], variable_by_name.(md, "μ".*name.(var_array)))
-    end
-
-    obj_expr = objective_function(md)
-    @objective(md, Min, -obj_expr)
-
+function auto_dual_bellman_operator(dhm::DecisionHazardModel, t::Int, lip::Real)
+    m = bellman_operator(dhm, t)
+    md = auto_dual_bellman_operator(m, lip)
     return md
 end
